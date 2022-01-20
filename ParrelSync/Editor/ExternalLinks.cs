18cb2f11930e3c930dcd52c5a5fcbafcb9506e96
brentatpeoplefun <82164796+brentatpeoplefun@users.noreply.github.com>
2022-01-20 14:43:51 -0600

    Fixed some some copy pasta goofs and made it clear this is forked from another project

--- ParrelSync/Editor/ExternalLinks.cs
+++ ParrelSync/Editor/ExternalLinks.cs
@@ -2,12 +2,12 @@
 {
     public class ExternalLinks 
     {
-        public const string RemoteVersionURL = "https://raw.githubusercontent.com/VeriorPies/brentatpeoplefun/master/VERSION.txt";
-        public const string Releases = "https://github.com/VeriorPies/brentatpeoplefun/releases";
-        public const string CustomArgumentHelpLink = "https://github.com/VeriorPies/brentatpeoplefun/wiki/Argument";
+        public const string RemoteVersionURL = "https://raw.githubusercontent.com/brentatpeoplefun/ParrelSync/master/VERSION.txt";
+        public const string Releases = "https://github.com/brentatpeoplefun/ParrelSync/releases";
+        public const string CustomArgumentHelpLink = "https://github.com/brentatpeoplefun/ParrelSync/wiki/Argument";
 
-        public const string GitHubHome = "https://github.com/VeriorPies/brentatpeoplefun/";
-        public const string GitHubIssue = "https://github.com/VeriorPies/brentatpeoplefun/issues";
-        public const string FAQ = "https://github.com/VeriorPies/brentatpeoplefun/wiki/Troubleshooting-&-FAQs";
+        public const string GitHubHome = "https://github.com/brentatpeoplefun/ParrelSync/";
+        public const string GitHubIssue = "https://github.com/brentatpeoplefun/ParrelSync/issues";
+        public const string FAQ = "https://github.com/brentatpeoplefun/ParrelSync/wiki/Troubleshooting-&-FAQs";
     }
 }
